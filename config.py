8fd68f03693d1a85acebb215c22260ad793f6595
lhamo012 <lhamo012@umn.edu>
2019-12-16 14:14:16 -0600

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DEBUG = True
 SECRET_KEY = "row the boat"
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 GOODREADS_API_KEY = 'cvLN3ZMV7bztS4vPEkwpg'
 GOODREADS_SECRET =  'E9xSffpYj3jKHvALOsttTJFQ9NWc3n511B6Yv0eq9A'
