1ff17c73d2961de712e5e39eb1a884f091bc80a3
lhamo012 <lhamo012@umn.edu>
2019-12-16 12:10:40 -0600

    removing finalproject folder

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+DEBUG = True
+SECRET_KEY = "row the boat"
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+GOODREADS_API_KEY = 'cvLN3ZMV7bztS4vPEkwpg'
+GOODREADS_SECRET =  'E9xSffpYj3jKHvALOsttTJFQ9NWc3n511B6Yv0eq9A'
