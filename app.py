2254dd7547961565f9add1ad3c726f3e02a7a177
lhamo012 <lhamo012@umn.edu>
2019-12-16 15:12:34 -0600

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 app.config.from_pyfile('config.py')
 
 datab = SQLAlchemy(app)
